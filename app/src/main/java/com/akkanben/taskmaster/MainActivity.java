05be5fb8d4decc7bd8ace0e5d416d5b15afb5ad7
Ben Mills <fuzzyllama@gmail.com>
2022-03-21 20:12:27 -0700

    Adds button listeners and intents for main activity

--- app/src/main/java/com/akkanben/taskmaster/MainActivity.java
+++ app/src/main/java/com/akkanben/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.akkanben.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addTaskButton = findViewById(R.id.button_main_add_task);
+        Button allTasksButton = findViewById(R.id.button_main_all_tasks);
+
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAddTaskIntent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(goToAddTaskIntent);
+            }
+        });
+
+        allTasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasksActivity.class);
+                startActivity(goToAllTasksIntent);
+            }
+        });
+
+
     }
 }

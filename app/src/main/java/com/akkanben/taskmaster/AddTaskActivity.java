9108c61478f4f8ce2794f1055d8da9db002e6d66
Ben Mills <fuzzyllama@gmail.com>
2022-03-21 20:15:39 -0700

    Fixes rename

--- app/src/main/java/com/akkanben/taskmaster/AddTaskActivity.java
+++ app/src/main/java/com/akkanben/taskmaster/AddTaskActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AddTask extends AppCompatActivity {
+public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

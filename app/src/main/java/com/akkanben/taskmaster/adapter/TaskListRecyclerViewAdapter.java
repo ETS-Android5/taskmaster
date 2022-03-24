f96df18d7f8622fdc7344730a7cdd200d1b3d98a
Ben Mills <fuzzyllama@gmail.com>
2022-03-23 19:18:45 -0700

    Adds clickable task that forwards to task details view

--- app/src/main/java/com/akkanben/taskmaster/adapter/TaskListRecyclerViewAdapter.java
+++ app/src/main/java/com/akkanben/taskmaster/adapter/TaskListRecyclerViewAdapter.java
@@ -1,6 +1,9 @@
 package com.akkanben.taskmaster.adapter;
 
+import static com.akkanben.taskmaster.activity.MainActivity.TASK_NAME_EXTRA_TAG;
+
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.akkanben.taskmaster.R;
 import com.akkanben.taskmaster.activity.MainActivity;
+import com.akkanben.taskmaster.activity.TaskDetailActivity;
 import com.akkanben.taskmaster.model.Task;
 
 import java.util.List;
@@ -38,6 +42,16 @@ public class TaskListRecyclerViewAdapter extends RecyclerView.Adapter {
         TextView taskFragmentTextView = holder.itemView.findViewById(R.id.text_view_task_list_fragment_task_name);
         String taskName = taskList.get(position).getTitle();
         taskFragmentTextView.setText(taskName);
+
+        View taskViewHolder = holder.itemView;
+        taskViewHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToTaskDetails = new Intent(callingActivity, TaskDetailActivity.class);
+                goToTaskDetails.putExtra(TASK_NAME_EXTRA_TAG, taskName);
+                callingActivity.startActivity(goToTaskDetails);
+            }
+        });
     }
 
     @Override

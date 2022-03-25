0dfb74dd025c3d65faddf175938f5666be529966
Ben Mills <fuzzyllama@gmail.com>
2022-03-24 17:04:14 -0700

    Adds extra methods for task status enum

--- app/src/main/java/com/akkanben/taskmaster/model/TaskStatus.java
+++ app/src/main/java/com/akkanben/taskmaster/model/TaskStatus.java
@@ -1,8 +1,31 @@
 package com.akkanben.taskmaster.model;
 
+import androidx.annotation.NonNull;
+
 public enum TaskStatus {
-    NEW,
-    ASSIGNED,
-    IN_PROGRESS,
-    COMPLETE
+    NEW("New"),
+    ASSIGNED("Assigned"),
+    IN_PROGRESS("In Progress"),
+    COMPLETE("Complete");
+
+    private final String taskString;
+
+    TaskStatus(String taskString) {
+        this.taskString = taskString;
+    }
+
+    public static TaskStatus fromString(String string) {
+        for (TaskStatus status : TaskStatus.values()) {
+            if (status.taskString.equals(string))
+                return status;
+        }
+        return null;
+    }
+    
+    @NonNull
+    @Override
+    public String toString() {
+        return taskString == null ? "" : taskString;
+    }
+
 }

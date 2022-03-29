7998cc9e9e7bf46ad6000b83e3d32252420ae6e2
Ben Mills <fuzzyllama@gmail.com>
2022-03-28 20:46:55 -0700

    Adds a username shared preferences clear to the main activity

--- app/src/androidTest/java/com/akkanben/taskmaster/activity/MainActivityTest.java
+++ app/src/androidTest/java/com/akkanben/taskmaster/activity/MainActivityTest.java
@@ -2,12 +2,20 @@ package com.akkanben.taskmaster.activity;
 
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.internal.data.model.ActionData;
@@ -17,6 +25,9 @@ import androidx.test.runner.AndroidJUnit4;
 
 import com.akkanben.taskmaster.R;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +41,38 @@ public class MainActivityTest {
 
     @Test
     public void mainActivityTest() {
+
+        ViewInteraction floatingSettingsButton = onView(
+                allOf(withId(R.id.main_activity_settings_floating_action_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        floatingSettingsButton.perform(click());
+
+        ViewInteraction usernameEditText = onView(
+                allOf(withId(R.id.edit_text_settings_activity_username),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        usernameEditText.perform(replaceText(""), closeSoftKeyboard());
+
+        ViewInteraction saveButton = onView(
+                allOf(withId(R.id.button_settings_activity_save), withText("Save"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        saveButton.perform(click());
+
+        pressBack();
         ViewInteraction textView = onView(
                 allOf(withText("TaskMaster"),
                         withParent(allOf(withId(androidx.appcompat.R.id.action_bar),
@@ -64,5 +107,23 @@ public class MainActivityTest {
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
         button2.check(matches(isDisplayed()));
+    }
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }

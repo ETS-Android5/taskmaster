b19356ee37bc4a5c0462d25da4072062e8f0cf60
Ben Mills <fuzzyllama@gmail.com>
2022-03-29 17:28:25 -0700

    Adds main taskmaster app to launch amplify when app starts

---
+++ app/src/main/java/com/akkanben/taskmaster/TaskMaster.java
@@ -0,0 +1,24 @@
+package com.akkanben.taskmaster;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.core.Amplify;
+
+public class TaskMaster extends Application {
+    public static final String TAG = "taskmaster_application_tag";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+        } catch (AmplifyException e) {
+            Log.e(TAG, "Error during initialization of Amplify: " + e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+}

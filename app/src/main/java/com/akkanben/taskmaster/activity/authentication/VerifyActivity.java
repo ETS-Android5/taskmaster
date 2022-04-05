5c349f8cf300cc81d6d22e2680f6909691d34d65
Ben Mills <fuzzyllama@gmail.com>
2022-04-04 20:36:31 -0700

    Adds verify feature

--- app/src/main/java/com/akkanben/taskmaster/activity/authentication/VerifyActivity.java
+++ app/src/main/java/com/akkanben/taskmaster/activity/authentication/VerifyActivity.java
@@ -2,15 +2,44 @@ package com.akkanben.taskmaster.activity.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.akkanben.taskmaster.R;
+import com.amplifyframework.core.Amplify;
 
 public class VerifyActivity extends AppCompatActivity {
+
+    public static final String TAG = "verify_activity_tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+        Intent callingIntent = getIntent();
+        String email = callingIntent.getStringExtra(SignUpActivity.SIGN_UP_EMAIL_TAG);
+        Button submitButton = findViewById(R.id.button_verify_activity_submit);
+        submitButton.setOnClickListener(view -> {
+           String verificationCode = ((EditText)findViewById(R.id.edit_text_verify_activity_verfication_number)).getText().toString();
+           Amplify.Auth.confirmSignUp(
+                   email,
+                   verificationCode,
+                   success -> {
+                       Log.i(TAG, "Verification Succeeded: " + success.toString());
+                       Intent goToLogInIntent = new Intent(VerifyActivity.this, LogInActivity.class);
+                       startActivity(goToLogInIntent);
+                   },
+                   failure -> {
+                       Log.i(TAG, "Vefication Failed: " + failure.toString());
+                       runOnUiThread(() -> Toast.makeText(VerifyActivity.this, "Verification Failed", Toast.LENGTH_SHORT));
+                   }
+           );
+        });
+
+
     }
 }

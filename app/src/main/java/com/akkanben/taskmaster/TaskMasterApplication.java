f1ef27459a684b3804bbf316c555fe855a0a909d
Ben Mills <fuzzyllama@gmail.com>
2022-04-11 19:45:33 -0700

    Adds text to speech button on task detail page.

--- app/src/main/java/com/akkanben/taskmaster/TaskMasterApplication.java
+++ app/src/main/java/com/akkanben/taskmaster/TaskMasterApplication.java
@@ -9,6 +9,7 @@ import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.geo.location.AWSLocationGeoPlugin;
+import com.amplifyframework.predictions.aws.AWSPredictionsPlugin;
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class TaskMasterApplication extends Application {
@@ -20,6 +21,7 @@ public class TaskMasterApplication extends Application {
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSPredictionsPlugin());
             Amplify.addPlugin(new AWSPinpointAnalyticsPlugin(this));
             Amplify.addPlugin(new AWSS3StoragePlugin());
             Amplify.addPlugin(new AWSLocationGeoPlugin());
--- app/src/main/java/com/akkanben/taskmaster/activity/TaskDetailActivity.java
+++ app/src/main/java/com/akkanben/taskmaster/activity/TaskDetailActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -14,17 +15,29 @@ import com.akkanben.taskmaster.R;
 import com.akkanben.taskmaster.utility.EnumUtility;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.TaskStatus;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class TaskDetailActivity extends AppCompatActivity {
 
     public static final String TAG = "task_detail_activity_tag";
+    private final MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+        setupTaskDetailsViaIntent();
+        setupTextToSpeechButton();
+    }
+
+    private void setupTaskDetailsViaIntent() {
         Intent callingIntent = getIntent();
         String taskNameString = null;
         String taskStatusString = null;
@@ -42,7 +55,7 @@ public class TaskDetailActivity extends AppCompatActivity {
             setupStatusBackgroundColor(status);
         }
 
-        TextView taskNameTextView = findViewById(R.id.task_detail_activity_task_title_text_view);
+        TextView taskNameTextView = findViewById(R.id.text_view_task_detail_activity_task_title);
         if (taskNameString != null)
             taskNameTextView.setText(taskNameString);
         else
@@ -70,16 +83,48 @@ public class TaskDetailActivity extends AppCompatActivity {
 
         if (taskAttachementKeyString != null && !taskAttachementKeyString.isEmpty()) {
             Amplify.Storage.downloadFile(
-                   taskAttachementKeyString,
-                   new File(getApplication().getFilesDir(), taskAttachementKeyString),
-                   success -> {
-                       ImageView taskImageView = findViewById(R.id.image_view_task_detail_attachment);
-                       taskImageView.setImageBitmap(BitmapFactory.decodeFile(success.getFile().getPath()));
-                   },
-                   failure -> {
-                       Log.e(TAG, "Unable to get image with S3 key because " + failure.getMessage());
-                   }
+                    taskAttachementKeyString,
+                    new File(getApplication().getFilesDir(), taskAttachementKeyString),
+                    success -> {
+                        ImageView taskImageView = findViewById(R.id.image_view_task_detail_attachment);
+                        taskImageView.setImageBitmap(BitmapFactory.decodeFile(success.getFile().getPath()));
+                    },
+                    failure -> {
+                        Log.e(TAG, "Unable to get image with S3 key because " + failure.getMessage());
+                    }
             );
+        }
+    }
+
+    private void setupTextToSpeechButton() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.floating_action_button_task_detail_activity_tts);
+        String textToSpeech = ((TextView)findViewById(R.id.text_view_task_detail_activity_task_title)).getText().toString()
+                + ", "
+                + ((TextView)findViewById(R.id.text_view_task_detail_description)).getText().toString();
+        floatingActionButton.setOnClickListener(v -> {
+                Amplify.Predictions.convertTextToSpeech(
+                        textToSpeech,
+                        success -> playTextToSpeechAudio(success.getAudioData()),
+                        failure -> Log.e(TAG, "Failed to convert text to speech")
+                );
+        });
+    }
+
+    private void playTextToSpeechAudio(InputStream inputStream) {
+        File audioMP3File = new File(getCacheDir(), "tts.mp3");
+
+        try (OutputStream out = new FileOutputStream(audioMP3File)) {
+            byte[] buffer = new byte[8 * 1_024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            mediaPlayer.reset();
+            mediaPlayer.setOnPreparedListener(MediaPlayer::start);
+            mediaPlayer.setDataSource(new FileInputStream(audioMP3File).getFD());
+            mediaPlayer.prepareAsync();
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error writing text-to-speech file", ioe);
         }
     }
 
